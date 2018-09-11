f64a70ebc989ea98cad0dfbaaf642f8eeef65bf7
HMIN <1316914661@qq.com>
2018-09-11 19:02:38 +0800

    简化react项目的源码

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+// PWA progressive web application
+// https协议的服务器上 断网可重连 缓存
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
