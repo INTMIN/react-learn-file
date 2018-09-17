81f4b456cd0ee87e036d7eff0503a9238490646d
HMIN <1316914661@qq.com>
2018-09-17 10:37:09 +0800

    在index里引入ToduList.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import TodoList from './TodoList';
 
 // PWA progressive web application
 // https协议的服务器上 断网可重连 缓存
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoList />, document.getElementById('root'));
 registerServiceWorker();
