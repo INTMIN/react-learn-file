d21ae93050910491e80a559b71d756c9bda973c2
HMIN <1316914661@qq.com>
2018-09-17 09:47:04 +0800

    注释App.js文件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 // 上面的那个等价于
 // import React from 'react'
 // const Component = React.Component
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello React
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello React
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
