f76caf5327783c2d51732fbbc12c276e5fe1fcd4
HMIN <1316914661@qq.com>
2018-09-18 11:49:00 +0800

    更新Prop Types 和defaultprops

--- my-app/src/TodoLitem.js
+++ my-app/src/TodoLitem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component{
 
@@ -7,15 +8,28 @@ class TodoItem extends Component{
         this.handleClick = this.handleClick.bind(this)
     }
     render() {
-        const {content} = this.props;
+        const {content, test} = this.props;
         return (
         <div onClick={this.handleClick}>
-            {content}
+            {test} - {content}
         </div>
-        )}
+        )
+    }
     handleClick() {
         const {deleteItem, index} = this.props
         deleteItem(index);
     }
 }
+// 类型校验
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.arrayOf(PropTypes.number, PropTypes.string),
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+// 默认值的设置
+TodoItem.defaultProps = {
+    test: 'MIN数据'
+}
+
 export default TodoItem;
