aef324f2e364c21ac312c39235e1d5e4481eb6db
HMIN <1316914661@qq.com>
2018-09-18 16:15:24 +0800

    更新对虚拟DOM的理解

--- my-app/src/TodoLitem.js
+++ my-app/src/TodoLitem.js
@@ -9,7 +9,7 @@ class TodoItem extends Component{
     }
     render() {
         const {content, test} = this.props;
-        // 顺序是JSX ->先生成JS对象也就是虚拟DOM -> 再生成真实DOM
+        // 顺序是JSX -> 通过底层creatElement方法-> 生成JS对象也就是虚拟DOM -> 再生成真实DOM
         return (
         <div onClick={this.handleClick}>
             {test} - {content}
