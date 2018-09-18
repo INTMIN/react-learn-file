56f38eefa64a390214338f0b49937ef6a055d173
HMIN <1316914661@qq.com>
2018-09-18 20:18:28 +0800

    更新了一个新的动画效果的组件

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,31 @@
-// import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import './style.css'
 // 上面的那个等价于
 // import React from 'react'
 // const Component = React.Component
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         Hello React
-//       </div>
-//     );
-//   }
-// }
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: true
+        }
+        this.handleToggole = this.handleToggole.bind(this);
+    }
+    
+  render() {
+    return (
+        <Fragment>
+      <div className={this.state.show ? 'show' :'hide'}>Hello React</div>
+      <button onClick={this.handleToggole}>toggle</button>
+      </Fragment>
+    )
+  }
+  handleToggole() {
+      this.setState({
+          show: this.state.show ? false : true
+      })
+  }
+}
 
-// export default App;
+export default App;
