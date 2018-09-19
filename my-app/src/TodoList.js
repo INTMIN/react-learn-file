dea35c4595195437b59310afc17654282d975cfa
HMIN <1316914661@qq.com>
2018-09-19 16:52:45 +0800

    更新了redux的用法

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,30 +1,25 @@
 import React, { Component } from 'react'
-import { Input } from 'antd';
-import { Button } from 'antd';
-import { List } from 'antd';
+import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
-
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+import store from './store';
   
  class TodoList extends Component {
-  render() {
-    return (
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        console.log(store.getState());
+    }
+    render() {
+      return (
       <div style={{marginTop: '10px',marginLeft:'10px'}}>
         <div>
-            <Input placeholder="Basic usage" style={{width: '300px', marginRight:'10px'}} />
+            <Input value={this.state.inputValue} placeholder="Basic usage" style={{width: '300px', marginRight:'10px'}} />
             <Button type="primary"> 提交</Button>
         </div>
         <List
             style={{marginTop: '10px', width: '300px'}}
             bordered
-            dataSource={data}
+            dataSource={this.state.list}
             renderItem={item => (<List.Item>{item}</List.Item>)}
     />
       </div>
