006002721b49772e8a8fdfdfba96aa58b693a8a5
HMIN <1316914661@qq.com>
2018-09-19 19:05:39 +0800

    redux实现input功能

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -3,28 +3,45 @@ import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import store from './store';
   
- class TodoList extends Component {
-    constructor(props){
-        super(props);
-        this.state = store.getState();
-        console.log(store.getState());
-    }
-    render() {
-      return (
-      <div style={{marginTop: '10px',marginLeft:'10px'}}>
-        <div>
-            <Input value={this.state.inputValue} placeholder="Basic usage" style={{width: '300px', marginRight:'10px'}} />
+    class TodoList extends Component {
+        constructor(props){
+            super(props);
+            this.state = store.getState();
+            this.handleInputChange = this.handleInputChange.bind(this);
+            this.handleStoreChange = this.handleStoreChange.bind(this);
+            store.subscribe(this.handleStoreChange);
+        }
+        render() {
+        return (
+          <div style={{marginTop: '10px',marginLeft:'10px'}}>
+            <div>
+                <Input 
+                    value={this.state.inputValue} 
+                    placeholder="Basic usage" 
+                    style={{width: '300px', marginRight:'10px'}} 
+                    onChange={this.handleInputChange}
+                />
             <Button type="primary"> 提交</Button>
+            </div>
+            <List
+                style={{marginTop: '10px', width: '300px'}}
+                bordered
+                dataSource={this.state.list}
+                renderItem={item => (<List.Item>{item}</List.Item>)}
+            />
         </div>
-        <List
-            style={{marginTop: '10px', width: '300px'}}
-            bordered
-            dataSource={this.state.list}
-            renderItem={item => (<List.Item>{item}</List.Item>)}
-    />
-      </div>
-    )
-  }
+        )
+    }
+    handleInputChange(e) {
+      const action = {
+        type: 'change_input_value',
+        value: e.target.value
+        }
+        store.dispatch(action);
+    }
+    handleStoreChange(){
+        this.setState(store.getState());
+    }
 }
 
 export default TodoList;
--- my-app/src/store/reducer.js
+++ my-app/src/store/reducer.js
@@ -3,6 +3,13 @@ const defaultState = {
     list: [1, 2]
 }
 
+// reducer 可以接收state，但是绝不能修改state所以需要拷贝一份
 export default (state = defaultState, action) => {
+    if (action.type === 'change_input_value'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    }
+    console.log(state, action);
     return state;
 }
