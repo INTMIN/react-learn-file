521edf184e83291e7971167dfcc4555ce5ee95f0
HMIN <1316914661@qq.com>
2018-09-19 12:11:04 +0800

    使用react  transition group 实现todolist动画

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './style1.css';
 // 上面的那个等价于
@@ -8,44 +8,46 @@ import './style1.css';
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            list: []
+        this.state = this.state = {
+            items: ['hello', 'world', 'click', 'me']
         }
         this.handleAddItem = this.handleAddItem.bind(this);
     }
+    handleAddItem() {
+        const newItems = this.state.items.concat([
+            prompt('Enter some text')
+          ]);
+          this.setState({items: newItems});
+    }
+    handleRemove(i) {
+        let newItems = this.state.items.slice();
+        newItems.splice(i, 1);
+        this.setState({items: newItems});
+    }
     
-  render() {
-    return (
-        <Fragment>
-          <TransitionGroup>
-            {
-                this.state.list.map((item, index) => {
-                    return (
-                        <CSSTransition
-                            timeout={1000}
-                            classNames='fade'
-                            unmountOnExit
-                            onEntered={(el) => {el.style.color='blue'}}
-                            appear={true}
-                            key={index}
-                        >
-                            <div>{item}</div>
-                        </CSSTransition>
-                    )  
-                })
-            }
-            <button onClick={this.handleAddItem}>toggle</button>
-            </TransitionGroup>
-      </Fragment>
+    
+    render() {
+        const items = this.state.items.map((item, i) => (
+            <CSSTransition
+                key={i}
+                classNames="example"
+                timeout={{ enter: 500, exit: 300 }}
+            >
+                <div onClick={() => this.handleRemove(i)}>
+                    {item}
+                </div>
+            </CSSTransition>
+        ));
+            return (
+                <div>
+                <button onClick={this.handleAddItem}>toggle</button>
+                <TransitionGroup>
+                    {items}
+                </TransitionGroup>
+            </div>
     )
   }
-  handleAddItem() {
-      this.setState((prevState) => {
-          return{
-            list: [...prevState.list, 'item']
-        }    
-      })
-  }
+  
 }
 
 export default App;
