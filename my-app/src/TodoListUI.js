ea838a55ad4c3c901290adc0082a5105aef9ec58
HMIN <1316914661@qq.com>
2018-09-20 16:24:04 +0800

    使用无状态组件

--- my-app/src/TodoListUI.js
+++ my-app/src/TodoListUI.js
@@ -24,28 +24,4 @@ const TodoListUI = (props) => {
     )
 }
 
-// class TodoListUI extends Component{
-//     render() {
-//         return (
-//             <div style={{marginTop: '10px',marginLeft:'10px'}}>
-//             <div>
-//                 <Input 
-//                     value={this.props.inputValue} 
-//                     placeholder="Basic usage" 
-//                     style={{width: '300px', marginRight:'10px'}} 
-//                     onChange={this.props.handleInputChange}
-//                 />
-//             <Button type="primary" onClick={this.props.handleBtnClick}> 提交</Button>
-//             </div>
-//             <List
-//                 style={{marginTop: '10px', width: '300px'}}
-//                 bordered
-//                 dataSource={this.props.list}
-//                 renderItem={(item, index) => (<List.Item onClick={(index) => {this.props.handItemDelete(index)}}>{item}</List.Item>)}
-//             />
-//         </div>
-//         )
-        
-//     }
-// }
 export default TodoListUI;
