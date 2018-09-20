982c55e29945aa6b81b38aef597a0bc4d1936674
HMIN <1316914661@qq.com>
2018-09-20 16:18:15 +0800

    todolist的UI组件的拆分

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { Input, Button, List } from 'antd';
 import 'antd/dist/antd.css';
 import store from './store';
 import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './store/actionCreators';
+import TodoListUI from './TodoListUI';
 
 
     class TodoList extends Component {
@@ -12,27 +12,18 @@ import { getInputChangeAction, getAddItemAction, getDeleteItemAction } from './s
             this.handleInputChange = this.handleInputChange.bind(this);
             this.handleStoreChange = this.handleStoreChange.bind(this);
             this.handleBtnClick = this.handleBtnClick.bind(this);
+            this.handItemDelete = this.handItemDelete.bind(this);
             store.subscribe(this.handleStoreChange);
         }
         render() {
         return (
-          <div style={{marginTop: '10px',marginLeft:'10px'}}>
-            <div>
-                <Input 
-                    value={this.state.inputValue} 
-                    placeholder="Basic usage" 
-                    style={{width: '300px', marginRight:'10px'}} 
-                    onChange={this.handleInputChange}
-                />
-            <Button type="primary" onClick={this.handleBtnClick}> 提交</Button>
-            </div>
-            <List
-                style={{marginTop: '10px', width: '300px'}}
-                bordered
-                dataSource={this.state.list}
-                renderItem={(item, index) => (<List.Item onClick={this.handItemDelete.bind(this, index)}>{item}</List.Item>)}
-            />
-        </div>
+        <TodoListUI 
+            inputValue={this.state.inputValue}
+            list={this.state.list}
+            handleInputChange={this.handleInputChange}
+            handleBtnClick={this.handleBtnClick}
+            handItemDelete={this.handItemDelete}
+        />
         )
     }
     handleInputChange(e) {
