fcf64c3ff33d57bda4066b6df8e2a890ae5a40a6
HMIN <1316914661@qq.com>
2018-09-21 16:40:16 +0800

    更新react-redux实现todoList功能

---
+++ my-app/src/TodoList.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const TodoList = (props) => {
+    const {inputValue, changeInputValue, handleClick, list} = props;
+    return (
+        <div>
+            <div>
+                <input value={inputValue} onChange={changeInputValue} />
+                <button onClick={handleClick}>提交</button>
+            </div>
+            <ul>
+                {
+                    list.map((item, index) => {
+                        return <li key={index}>{item}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        inputValue: state.inputValue,
+        list: state.list
+    }
+}
+
+// store.dispatch, props
+const mapDispatchToProps = (dispatch) => {
+    return {
+        changeInputValue(e) {
+            const action = {
+                type: 'change_input_value',
+                value: e.target.value
+            }
+            dispatch(action);
+        },
+
+        handleClick() {
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action);
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
