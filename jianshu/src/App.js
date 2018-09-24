2262f37f179bcafdb7df5c5aa0ecf12a75d89096
HMIN <1316914661@qq.com>
2018-09-24 12:05:35 +0800

    更新使用yarn来管理依赖包

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import styled,{ css } from 'styled-components';
+
+
+
+
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
 
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='dell'>
         Hello React
+      
+      <Button>Normal Button</Button>
+      <Button primary>Primary Button</Button>
+    
       </div>
     );
   }
