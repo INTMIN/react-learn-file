6821f387144dbc6a21862c8a999cbf47d3ac7354
HMIN <1316914661@qq.com>
2018-09-27 11:08:25 +0800

    更新路由功能

--- jianshu/src/App.js
+++ jianshu/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './common/header';
 import store from './store';
 
@@ -8,7 +9,15 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header />
+      <div>
+      <Header />
+        <BrowserRouter>
+        <div>
+          <Route path='/' exact render={()=><div>home</div>}></Route>
+          <Route path='/detail' exact render={()=><div>detail</div>}></Route>
+        </div>
+        </BrowserRouter>
+      </div>  
       </Provider>
     );
   }
