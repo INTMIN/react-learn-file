0db2c7d1fac6ba6b2a987963748cb98f40f6cf6b
MIN <1316914661@qq.com>
2019-04-12 14:45:09 +0800

    代码优化

--- jianshu/src/pages/home/components/List.js
+++ jianshu/src/pages/home/components/List.js
@@ -7,26 +7,26 @@ import {Link} from 'react-router-dom';
 class List extends PureComponent {
 render(){
     const { list, getMoreList, page } = this.props;
-        return (
-            <div>
-            {
-                list.map((item, index) => {
-                    return (
-                        <Link key={index} to={'./detail/' + item.get('id')}>
-                        <ListItem >
-                            <img className='pic' src={item.get('imgUrl')} alt="240"/>
-                            <ListInfo>
-                                <h3 className='title'>{item.get('title')}</h3>
-                                <p className='desc'>{item.get('desc')}</p>
-                            </ListInfo>
-                        </ListItem>
-                        </Link>
-                    );
-                })
-            }
-            <LoadMore onClick={() => getMoreList(page)}>更多内容</LoadMore> 
-            </div>
-        )
+	return (
+		<div>
+			{
+				list.map((item, index) => {
+					return (
+						<Link key={index} to={'./detail/' + item.get('id')}>
+							<ListItem >
+								<img className='pic' src={item.get('imgUrl')} alt="240" />
+								<ListInfo>
+									<h3 className='title'>{item.get('title')}</h3>
+									<p className='desc'>{item.get('desc')}</p>
+								</ListInfo>
+							</ListItem>
+						</Link>
+					);
+				})
+			}
+			<LoadMore onClick={() => getMoreList(page)}>更多内容</LoadMore>
+		</div>
+	);
     }
 }
 
@@ -37,7 +37,7 @@ const mapStateToProps = (state) => ({
 
 const mapDispatch = (dispatch) => ({
     getMoreList(page) {
-        dispatch(actionCreators.getMoreList(page))
+		dispatch(actionCreators.getMoreList(page));
     }
 });
 export default connect(mapStateToProps,mapDispatch)(List);
--- jianshu/src/pages/home/components/Recommend.js
+++ jianshu/src/pages/home/components/Recommend.js
@@ -1,24 +1,24 @@
 import React, { PureComponent } from 'react';
 import {connect} from 'react-redux';
-import {RecommendWrapper, RecommendItem} from '../style'
+import { RecommendWrapper, RecommendItem } from '../style';
 
 class Recommend extends PureComponent {
 render(){
     const {list} = this.props;
-        return (
-            <RecommendWrapper>
-            {
-                list.map((item) => {
-                    return <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')}/>
-                })
-            }
-            </RecommendWrapper>
-            )
+	return (
+		<RecommendWrapper>
+			{
+				list.map((item) => {
+					return <RecommendItem imgUrl={item.get('imgUrl')} key={item.get('id')} />;
+				})
+			}
+		</RecommendWrapper>
+	);
         }
 }
 
 const mapStateToProps = (state) => ({
-    list: state.getIn(['home','recommendList'])
-})
+	list: state.getIn(['home', 'recommendList'])
+});
 
 export default connect(mapStateToProps, null)(Recommend);
