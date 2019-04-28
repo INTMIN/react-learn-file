ad7f390953496c8b84c597b95bf51a4301ee159b
MIN <1316914661@qq.com>
2019-04-28 16:47:16 +0800

    redux

--- ts/tstest/src/components/Hello.tsx
+++ ts/tstest/src/components/Hello.tsx
@@ -1,23 +1,29 @@
 import * as React from 'react';
 import './Hello.css'
 
-export interface Props{
-  name:string;
-  enthusiasmLevel?:number;
+export interface Props {
+  name: string;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
 }
 
-function Hello ({name,enthusiasmLevel = 1}:Props){
-  if(enthusiasmLevel <=0){
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if (enthusiasmLevel <= 0) {
     throw new Error('You could be a little more enthusiastic. :D');
   }
 
   return (
-    <div className='hello'>
-      <div className='greeting'>
+    <div className="hello">
+      <div className="greeting">
         Hello {name + getExclamationMarks(enthusiasmLevel)}
       </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hello;
