84d8f309ac142406591fff5baa8911b05cdf233d
INTMIN <min_world@qq.com>
2022-01-10 18:43:11 +0800

    feat: 添加go关于echo的库以及基础文件，路由或者接口

---
+++ go-react/go-echo/go.mod
@@ -0,0 +1,8 @@
+module firstapp
+
+go 1.13
+
+require (
+	github.com/labstack/echo/v4 v4.6.2
+	golang.org/x/tools/gopls v0.7.4 // indirect
+)
---
+++ go-react/go-echo/server.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`s
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func saveUser(c echo.Context) error {
+	// Get name and email
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+name+", email:"+email)
+}
+
+func main() {
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.POST("/users", saveUser)
+	e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+	e.GET("/show", show)
+	e.Logger.Fatal(e.Start(":1323"))
+
+}
